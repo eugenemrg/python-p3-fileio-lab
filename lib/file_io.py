1a31c518a430b54e050867adb6b875a85408508f
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-08-25 15:27:45 +0300

    Completed assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,12 @@
 def write_file(file_name, file_content):
-    pass
+    a_file_name = open(f"{file_name}.txt", mode='w', encoding='utf-8')
+    a_file_name.write(file_content)
+    a_file_name.close()
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", encoding='utf-8', mode='a') as a_file_name:
+        a_file_name.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", encoding='utf-8', mode='r') as a_file_name:
+        return a_file_name.read()
